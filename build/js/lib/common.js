ac74c7c59b2210f143ac912912d361d2fb472f00
watch1k <host.sf@gmail.com>
2016-10-28 22:54:23 +0300

    initial commit

--- build/js/lib/common.js
+++ build/js/lib/common.js
@@ -1,19 +1,44 @@
 /* Common JS */
-$(document).ready(function(){
+$(document).ready(function () {
 
 	//for IE9
 	svg4everybody();
 
 	// Clear placeholder
-	(function() {
+	(function () {
 		var el = $('input, textarea');
-		el.focus(function(){
-			$(this).data('placeholder',$(this).attr('placeholder'));
-			$(this).attr('placeholder','');
+		el.focus(function () {
+			$(this).data('placeholder', $(this).attr('placeholder'));
+			$(this).attr('placeholder', '');
 		});
-		el.blur(function(){
-			$(this).attr('placeholder',$(this).data('placeholder'));
+		el.blur(function () {
+			$(this).attr('placeholder', $(this).data('placeholder'));
 		});
 	}());
+
+	(function () {
+		var tabs = $('.js-tabs');
+		if (tabs.length) {
+			var tabsItem = $('.js-tabs-content').children(),
+				tabsButton = $('.js-tabs-nav').children(),
+				currentHeight = 0;
+
+			currentHeight = tabsItem.outerHeight();
+			tabsItem.next().hide();
+			tabs.css({height: currentHeight});
+
+			tabsButton.on('click', function () {
+				var index = $(this).index(),
+					tabsHeight = 0;
+				tabsButton.removeClass('is-active');
+				$(this).addClass('is-active');
+				tabsItem.fadeOut().promise().done(function () {
+					tabsHeight = tabsItem.eq(index).outerHeight();
+					tabs.css({height: tabsHeight});
+					tabsItem.eq(index).fadeIn();
+				});
+			});
+		}
+	})();
 
 });
